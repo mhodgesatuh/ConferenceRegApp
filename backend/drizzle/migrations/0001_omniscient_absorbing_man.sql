ff2bae81cb90c70212793d2c664ac3deb27cf41f
mhodges <mhodges2@gmail.com>
2025-06-14 13:09:06 -1000

    Redact drizzle migration directory

--- backend/drizzle/migrations/0001_omniscient_absorbing_man.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX `idx_people_cancelled_attendance` ON `people` (`cancelled_attendance`);--> statement-breakpoint
-CREATE INDEX `idx_people_is_attendee` ON `people` (`is_attendee`);--> statement-breakpoint
-CREATE INDEX `idx_people_is_monitor` ON `people` (`is_monitor`);--> statement-breakpoint
-CREATE INDEX `idx_people_is_organizer` ON `people` (`is_organizer`);--> statement-breakpoint
-CREATE INDEX `idx_people_is_presenter` ON `people` (`is_presenter`);--> statement-breakpoint
-CREATE INDEX `idx_people_is_proxy` ON `people` (`is_proxy`);--> statement-breakpoint
-CREATE INDEX `idx_people_has_proxy` ON `people` (`has_proxy`);--> statement-breakpoint
-CREATE INDEX `idx_people_proxy_id` ON `people` (`proxy_id`);--> statement-breakpoint
-CREATE INDEX `idx_validation_tables_value` ON `validation_tables` (`value`);
