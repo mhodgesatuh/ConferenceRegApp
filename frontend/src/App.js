1454d2e2e99357b8b52a61d6f0354b1c29c5672b
Michael Hodges <mhodgesatuh@users.noreply.github.com>
2025-07-12 11:14:41 -1000

    chore: remove compiled js files

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-// frontend/src/App.tsx
-import React from 'react';
-import RegForm from './RegForm';
-const App = () => {
-    return (_jsxs("div", { className: "app", children: [_jsx("h1", { children: "Conference Registration" }), _jsx(RegForm, {})] }));
-};
-export default App;
