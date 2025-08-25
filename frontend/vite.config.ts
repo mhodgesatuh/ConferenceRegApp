765e96cb88be659432123b8a2db9def7be14eb15
Michael Hodges <mhodgesatuh@users.noreply.github.com>
2025-08-24 17:22:30 -1000

    feat: make frontend https optional

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,27 +4,34 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ command }) => {
     const isDev = command === 'serve';
+
+    const keyPath = path.resolve('/certs/localhost-key.pem');
+    const certPath = path.resolve('/certs/localhost.pem');
+    const httpsOptions =
+        fs.existsSync(keyPath) && fs.existsSync(certPath)
+            ? {
+                  key: fs.readFileSync(keyPath),
+                  cert: fs.readFileSync(certPath),
+              }
+            : undefined;
 
     return {
         plugins: [react(), tsconfigPaths()],
         server: isDev
             ? {
-                host: true,
-                port: 3000,
-                https: {
-                    key: fs.readFileSync(path.resolve('/certs/localhost-key.pem')),
-                    cert: fs.readFileSync(path.resolve('/certs/localhost.pem')),
-                },
-                proxy: {
-                    '/api': {
-                        target: 'https://conference-backend:5000',
-                        changeOrigin: true,
-                        secure: false,
-                    },
-                },
-            }
+                  host: true,
+                  port: 3000,
+                  https: httpsOptions,
+                  proxy: {
+                      '/api': {
+                          target: 'https://conference-backend:5000',
+                          changeOrigin: true,
+                          secure: false,
+                      },
+                  },
+              }
             : undefined,
         build: {
             outDir: 'dist',
